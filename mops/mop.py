40661b8a86b3962c83676b8445950742279003ef
josh-work <jdickman@cenic.org>
2021-03-29 14:39:08 -0700

    separated mop/cd yaml

--- mops/mop.py
+++ mops/mop.py
@@ -7,6 +7,7 @@ import shutil
 import sys
 import argparse
 import pickle
+import os
 
 #pylint: disable=import-error
 sys.path.append('../projects/atl_cal')
@@ -19,13 +20,13 @@ class CreateMOPs:
     def __init__(self, args):
         """Create MOP/CD pages, link to Jira and create Calendar event (if applicable)."""
 
-        with open("mop.yaml") as file:
-            self.mop_file = yaml.full_load(file)
+        with open(f"{args.mop_type}.yaml") as file:
+            self.yaml_file = yaml.full_load(file)
 
         if args.username:
             username = args.username
         else:
-            with open(self.mop_file['username_file_path']) as file2:
+            with open(self.yaml_file['username_file_path']) as file2:
                 usernames = yaml.full_load(file2)
             username = usernames['cas']
 
@@ -36,30 +37,30 @@ class CreateMOPs:
     def test_vars(self):
         """Make sure variables are set in YAML"""
 
-        if not self.mop_file['page_title']:
+        if not self.yaml_file['page_title']:
             print('\n\tEnter Title\n')
             sys.exit(1)
         else:
-            self.page_title = self.mop_file['page_title']
+            self.page_title = self.yaml_file['page_title']
 
-        if not self.mop_file['parent_page_id']:
+        if not self.yaml_file['parent_page_id']:
             print('\n\tEnter Parent Page ID\n')
             sys.exit(1)
         else:
-            self.parent_page_id = self.mop_file['parent_page_id']
+            self.parent_page_id = self.yaml_file['parent_page_id']
 
-        if not self.mop_file['ticket']:
+        if not self.yaml_file['ticket']:
             print('\n\tEnter Ticket\n')
             sys.exit(1)
         else:
-            self.ticket = self.mop_file['ticket']
+            self.ticket = self.yaml_file['ticket']
 
     def create_mop(self):
         """Create MOP from Jinja template, launch confluence & jira methods."""
 
         env = Environment(loader=FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
         template = env.get_template("mop-gen.j2")
-        self.page_body = template.render(self.mop_file)
+        self.page_body = template.render(self.yaml_file)
 
         self.test_vars()
         print(f'\n\tCreating MOP:\n\n\t\tTitle: {self.page_title}\n\t\tTicket: {self.ticket}\n\t\tJira Link: {self.args.link}\n')
@@ -73,10 +74,10 @@ class CreateMOPs:
 
         env = Environment(loader=FileSystemLoader('.'), trim_blocks=True, lstrip_blocks=True)
         template = env.get_template("cd-gen.j2")
-        self.page_body = template.render(self.mop_file)
+        self.page_body = template.render(self.yaml_file)
 
         self.test_vars()
-        print(f'\n\tCreating Change Doc:\n\n\t\tTitle: {self.page_title}\n\t\tTicket: {self.ticket}\n\t\tJira Link: {self.mop_file["link"]}\n')
+        print(f'\n\tCreating Change Doc:\n\n\t\tTitle: {self.page_title}\n\t\tTicket: {self.ticket}\n\t\tJira Link: {self.args.link}\n')
 
         self.update_confluence()
         #pylint: disable=no-member
@@ -104,12 +105,12 @@ class CreateMOPs:
     def create_calendar_event(self):
         """Create Internal Change event for CD."""
 
-        if self.mop_file['cd']['calendar']:
+        if self.yaml_file['calendar']:
 
-            start_time = str(self.mop_file['cd']['start_time'])
-            end_time = str(self.mop_file['cd']['end_time'])
-            day = self.mop_file['cd']['start_day']
-            title = self.mop_file['ticket'] + ': ' + self.mop_file['page_title']
+            start_time = str(self.yaml_file['start_time'])
+            end_time = str(self.yaml_file['end_time'])
+            day = self.yaml_file['start_day']
+            title = self.yaml_file['ticket'] + ': ' + self.yaml_file['page_title']
 
             print(f'\tCreating Internal Change entry:\n\n\t\tDay: {day}\n\t\tStart: {start_time}\n\t\tEnd: {end_time}\n')
             CalendarStuff().create_event(start_time, end_time, day, title)
@@ -117,8 +118,10 @@ class CreateMOPs:
     def move_yaml(self):
         """Copy YAML to repo"""
 
-        print(f'\tMoving YAML to storage: {self.mop_file["mop_repo"]}')
-        shutil.copy(self.mop_file['mop_directory'], self.mop_file['mop_repo'] +  self.page_title + '.yaml')
+        print(f'\tMoving YAML to storage: {self.yaml_file["mop_repo"]}')
+        print(os.path.dirname(__file__))
+        shutil.copy(os.path.dirname(__file__) + '/' + self.args.mop_type + '.yaml',
+                    self.yaml_file['mop_repo'] + self.args.mop_type + '/' +  self.page_title + '.yaml')
 
 
 if __name__ == '__main__':
