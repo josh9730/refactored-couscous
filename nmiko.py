e3da482deb2e3010062580ea539bf58287c88c13
josh-work <jdickman@cenic.org>
2021-08-20 12:52:56 -0700

    nmiko GP script

--- nmiko.py
+++ nmiko.py
@@ -1,3 +1,5 @@
+''' General purpose netmiko script'''
+
 from netmiko import ConnectHandler
 import pyotp
 import keyring
@@ -14,22 +16,22 @@ first_factor = keyring.get_password('mfa',user)
 otp_secret = keyring.get_password('otp',user)
 otp = pyotp.TOTP(otp_secret)
 
-xr_devices = [
-    # 'sac-agg4',
-    # 'tri-agg2',
-    # 'sfo-agg4',
-    # 'oak-agg8',
-    # 'svl-agg8',
-    # 'lax-agg8',
-    # 'tus-agg8',
-    # 'sdg-agg4',
-    # 'riv-agg8',
-    # 'slo-agg4',
-    # 'fre-agg4',
+xr_bb_devices = [
+    'sac-agg4',
+    'tri-agg2',
+    'sfo-agg4',
+    'oak-agg8',
+    'svl-agg8',
+    'lax-agg8',
+    'tus-agg8',
+    'sdg-agg4',
+    'riv-agg8',
+    'slo-agg4',
+    'fre-agg4',
     'frg-agg4'
 ]
 
-junos_devices = [
+junos_bb_devices = [
     'lax-agg10',
     'svl-agg10',
     'pdc-agg1',
@@ -38,52 +40,57 @@ junos_devices = [
     'bak-agg6'
 ]
 
+# use '|junos' or '|iosxr' for device_type
+device_list = [
+    'pdc-agg1|junos',
+    'cor-agg8|junos',
+    'sol-agg4|junos',
+    'bak-agg6|junos'
+]
+
 xr_show_commands = [
     # 'show bgp ipv6 unicast sum | utility egrep -B1 "Active|Connect"',
     'show run interface lo0'
 ]
 
 junos_show_commands = [
-    'show bgp summary | match "Active|Connect"'
+    'show system firmware | match "i40e-NVM',
+    'show bgp summary | match "137.164.16.6"'
 ]
 
-
-def connect(type, host, user, passwd):
+def connect(device_type, device_name, user, passwd):
     connection = ConnectHandler(
-    device_type = type,
-    host = host,
-    username = user,
-    password = passwd)
+        device_type = device_type,
+        host = device_name,
+        username = user,
+        password = passwd
+    )
+
     return connection
-
-def run_commands(device, connection, commands):
-    device_result = [f"\n\n\n{'='*20} {device} {'='*20}"] #header
-    command_result = connection.send_command(commands)
-    device_result.append(f"\n{'='*3} {commands} {'='*3}\n") # mini-header for the command input
-    device_result.append(command_result)
-
-    return device_result
 
 def main():
 
-    for i in xr_devices:
-        connection = connect('cisco_xr', i, user, first_factor + otp.now())
-        print(f'\t\t{i}')
-        for j in xr_show_commands:
-            a = connection.send_command(j)
+    for device in device_list:
+        device_name, device_type = device.split('|')
 
-            print(f'''
-            =======================================
-            |                                     |
-            |                                     |
-            |          {a}                        |
-            |                                     |
-            |                                     |
-            =======================================
-            '''
-            )
+        if device_type == 'junos':
+            device_type = 'juniper_junos'
+            commands_list = junos_show_commands
 
-        connection.disconnect()
+        elif device_type == 'iosxr':
+            device_type = 'cisco_xr'
+            commands_list = xr_show_commands
+
+        start_time = time.time()
+        connection = connect(device_type, device_name, user, first_factor + otp.now())
+
+        print('\n', '=' * 20, device_name.upper(), '=' * 20, '\n')
+        for command in commands_list:
+            print(f"COMMAND: '{command}")
+            print(connection.send_command(command), '\n\n')
+
+        time.sleep(30 - (time.time() - start_time))
+
 
 if __name__ == '__main__':
     main()
