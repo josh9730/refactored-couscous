1a50f4a08d973f3f3ddc5158a7ded71efc66f4e0
josh-work <jdickman@cenic.org>
2021-10-11 10:46:27 -0700

    panel job 0.1

--- jobs/devices.py
+++ jobs/devices.py
@@ -26,7 +26,7 @@ class CreatePanelPair(Job):
     rack_1 = ObjectVar(
         model = Rack,
         query_params = {
-            'site': '$site_name'
+            'name': '$site_name'
         }
     )
     rack_a_pos = IntegerVar(
