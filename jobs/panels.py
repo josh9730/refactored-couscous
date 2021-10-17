27b5924abb9a49e893bbe07de7e5668cb5634203
josh-work <jdickman@cenic.org>
2021-10-16 18:08:16 -0700

    panel run 0.1

--- jobs/panels.py
+++ jobs/panels.py
@@ -179,7 +179,7 @@ class JumperCassette(Job):
     )
     # panel_name = Device.objects.get(id=panel_1[0]).name
     kids = panel_1[0].get_children()
-    panel_name = panel_1[0].name
+    # panel_name = panel_1[0].name
     cassette_1 = ObjectVar(
         label = 'Cassette A',
         model = Device,
