176e4bc11fd2503b858bd86ac53f91c61f38629b
josh-work <jdickman@cenic.org>
2022-01-24 18:25:44 -0800

    resources fix

--- resources.py
+++ resources.py
@@ -1,5 +1,7 @@
 from datetime import datetime
+import numpy as np
 import argparse
+import holidays
 
 from tools.utils import jira_login
 
@@ -11,35 +13,63 @@ hours = timetracking.originalEstimateSeconds
 """
 
 
-def check_date(date):
-    # Date must be in isoformat
+def holidays_list():
+    """Return list of holidays for current and upcoming year"""
+    year = datetime.today().year
+    ca_holidays = holidays.CountryHoliday(
+        "US", prov=None, state="CA", years=[year, year + 1]
+    )
+    return [i.isoformat() for i in ca_holidays]
 
+
+def num_weekdays(date1, date2):
+    """Count of business days between date1, date2 less holidays."""
+    return np.busday_count(date1, date2, holidays=holidays_list())
+
+
+def new_org_est(tic_start, end_date, new_hours):
+    """Return Original Estimate based on new hours
+    gets new hours/day between today - end_date
+    multiplies that out over the days start date - end date
+    """
+    today = datetime.today().strftime("%Y-%m-%d")
+    days_remaining = num_weekdays(today, end_date) + 1
+    hrs_remaining = new_hours / days_remaining
+    total_days = num_weekdays(tic_start, end_date) + 1
+    return int(hrs_remaining * total_days)
+
+
+def check_date(date):
+    """Check date string format"""
     try:
         date = datetime.fromisoformat(date)
     except ValueError:
-        print("\nDate Error: Date must be provided in YYYY-MM-DD format.\n")
+        print("\nDate Error: Date must be in YYYY-MM-DD or MM/DD/YYYY format.\n")
+
+
+def convert_date(date):
+    """Convert MM/DD/YYYY -> YYYY-MM-DD"""
+    try:
+        month, day, year = date.split("/")
+        date = f"{year}-{month}-{day}"
+    except ValueError:
+        print("\nDate Error: Date must be in YYYY-MM-DD or MM/DD/YYYY format.\n")
+    return date
 
 
 def get_ticket(jira, args):
-    # get start_date, end_date, orginal_estimate from ticket
-
+    """Return start_date, end_date, orginal_estimate from ticket."""
     ticket = jira.get_issue(
-        args.ticket, fields=["customfield_10410", "customfield_10411", "timetracking"]
+        args.ticket, fields=["customfield_10410, customfield_10411"]
     )
     start = ticket["fields"]["customfield_10410"]
     end = ticket["fields"]["customfield_10411"]
 
-    try:
-        hrs = ticket["fields"]["timetracking"]["originalEstimateSeconds"] / 3600
-    except KeyError:
-        hrs = 0
-
-    return start, end, int(hrs)
+    return start, end
 
 
 def update_ticket(jira, ticket, start, end, org_est, rem_est):
-    # update start_date, end_date, estimates, and make sure ticket is In Progress
-
+    """Update start_date, end_date, estimates and check ticket is In Progress"""
     jira.issue_update(
         ticket,
         {
@@ -55,42 +85,27 @@ def update_ticket(jira, ticket, start, end, org_est, rem_est):
 
 
 def main(args):
-    check_date(args.end)
     jira = jira_login()
-    tic_start, tic_end, tic_hrs = get_ticket(jira, args)
+    tic_start, tic_end = get_ticket(jira, args)
+
+    if "/" in args.end:
+        args.end = convert_date(args.end)
     check_date(args.end)
 
     if args.start:
         """new ticket being tracked for resources"""
         update_ticket(jira, args.ticket, args.start, args.end, args.hours, 0)
 
-    elif args.end == tic_end and args.hours > 0:
-        """adjust hours with same end date
-
-        original estimate = tic_hrs + args.hours
-        remaining estimate = args.hours
-        """
-        org_est = int(tic_hrs + args.hours)
-        rem_est = int(args.hours)
-        update_ticket(jira, args.ticket, tic_start, tic_end, org_est, rem_est)
-
     elif args.end > tic_end and args.hours == 0:
         """adjust end date with same hours
 
         end date = args.end
         """
-        update_ticket(jira, args.ticket, tic_start, args.end, tic_hrs, 0)
+        update_ticket(jira, args.ticket, tic_start, args.end, args.hours, 0)
 
-    elif args.end > tic_end and args.hours > 0:
-        """adjust end date with additional hours
-
-        original estimate = tic_hrs + args.hours
-        remaining estimate = args.hours
-        end date = args.end
-        """
-        org_est = int(tic_hrs + args.hours)
-        rem_est = int(args.hours)
-        update_ticket(jira, args.ticket, tic_start, args.end, org_est, rem_est)
+    elif args.hours > 0:
+        org_est = new_org_est(tic_start, args.end, args.hours)
+        update_ticket(jira, args.ticket, tic_start, args.end, org_est, 0)
 
     else:
         print("\nSupplied End Date must be >= Current End and Hours must be >= 0")
