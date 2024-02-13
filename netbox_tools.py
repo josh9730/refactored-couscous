e1dabe50a987a9ebc44a69f25f6d303456fd6f8d
Josh <jdickman@cenic.org>
2024-02-13 13:51:00 -0800

    extend netbox_tools.py

--- netbox_tools.py
+++ netbox_tools.py
@@ -3,20 +3,32 @@ import csv
 import keyring
 import pynetbox
 import requests
+import typer
+from rich import print
+from rich.prompt import Prompt
+
+app = typer.Typer(add_completion=False)
 
 
-def get_auth():
+def get_auth() -> tuple[str, str]:
     url = keyring.get_password("netbox", "url")
+    if not url:
+        url = Prompt.ask("Enter NetBox URL")
+
     api_key = keyring.get_password("netbox", "api")
+    if not api_key:
+        api_key = Prompt.ask("Enter NetBox API Token", password=True)
+
     return url, api_key
 
 
-def get_pynb():
+def get_pynb() -> pynetbox:
     url, api_key = get_auth()
     return pynetbox.api(url, token=api_key)
 
 
-def post_asns():
+@app.command()
+def post_asns() -> None:
     url, api_key = get_auth()
     url = url + "/api/"
     headers = {
@@ -54,5 +66,67 @@ def post_asns():
                 print("failed: ", asn[0])
 
 
+@app.command()
+def remove_all_devices() -> None:
+    """Remove all Devices in NetBox one-by-one to avoid 504s."""
+    nb = get_pynb()
+    devices = nb.dcim.devices.all()  # can't bulk delete without a 504
+    for device in list(devices):
+        try:
+            device.delete()
+        except pynetbox.RequestError:
+            print(f"[red]Failed to delete {device}, re-attempt manually.")
+
+
+@app.command()
+def bulk_add_devices() -> None:
+    """Create new devices in NetBox from a CSV file. Failures are dumped to a new csv for analysis"""
+
+    def add_device(device: list[list[str]]) -> None:
+        site = nb.dcim.sites.get(name=device[9])
+        _ = nb.dcim.devices.create(
+            name=device[0],
+            role=nb.dcim.device_roles.get(name=device[1]).id,
+            tenant=nb.tenancy.tenants.get(name=device[2]).id,
+            device_type=nb.dcim.device_types.get(model=device[4]).id,
+            status=device[8].lower(),
+            site=site.id,
+            rack=nb.dcim.racks.get(name=device[11], site=site.slug).id,
+            position=device[12],
+            face=device[13].lower(),
+            comments=device[15],
+            custom_fields={
+                "deployment_ticket": device[16],
+                "facility_device_id": device[17],
+                "oob_number": device[18],
+            },
+        )
+
+    filename = "devices.csv"
+    failure_file = "failures.csv"
+    failed_devices = []
+
+    nb = get_pynb()
+    try:
+        with open(filename) as f:
+            devices = csv.reader(f)
+            for device in devices:
+                if device[0] == "name":  # header row
+                    failed_devices.append(device)
+                    continue
+                try:
+                    add_device(device)
+                except pynetbox.RequestError:
+                    failed_devices.append(device)
+                    print(f"[bold red]Failed to upload {device[0]}, re-attempt manually.")
+
+    except FileNotFoundError:
+        print(f"[bold red]Unable to find file: [white]{filename}")
+
+    with open(failure_file, "w") as f:
+        write = csv.writer(f)
+        write.writerows(failed_devices)
+
+
 if __name__ == "__main__":
-    post_asns()
+    app()
